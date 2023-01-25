84daa0f2452ac815297392ada6f7f3da85c55e2a
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-01-25 19:00:57 -0300

    organized files and folders

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+import { slides } from "./slides";
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
